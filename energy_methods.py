e91d551e46b1dad8f09c2ea9a11b0cc3b03b00bc
Daniel Pineda <89486342+DanielP520@users.noreply.github.com>
2023-06-06 09:53:22 -0400

    Update energy_methods.py

--- energy_methods.py
+++ energy_methods.py
@@ -1,6 +1,12 @@
 import pyrosetta
 
 def get_chain_energy(pose: pyrosetta.Pose, chain: int) -> float:
+    '''
+    Returns total energy for a chain in pose.
+    :param pose: Pyrosetta.Pose object
+    :param chain: Chain ID defined by rosetta
+    :return: float of sum of total score of per residue energy in a given chain.
+    '''
     total_energy = 0
     eng: pyrosetta.rosetta.core.scoring.Energies = pose.energies()
     chain_start = pose.chain_begin(chain)
@@ -13,6 +19,14 @@ def get_chain_energy(pose: pyrosetta.Pose, chain: int) -> float:
 
 def residue_energies_by_residue_type_list(pose: pyrosetta.Pose, score_fxn: pyrosetta.ScoreFunction,
                                           score_type: str, rosetta_index: int) -> dict:
+    '''
+    Returns dictionary of per residue energies separated by score_type, always includes total energy.
+    :param pose: Pyrosetta.Pose
+    :param score_fxn:  Pyrosetta.score_function
+    :param score_type: String of score_type separated by " "
+    :param rosetta_index: Int correspoding to residue in Rosetta Numbering
+    :return: dict of scores based on score_type.
+    '''
     working_pose: pyrosetta.Pose = pose.clone()
     score_fxn.score(working_pose)
     eng: pyrosetta.rosetta.core.scoring.Energies = working_pose.energies()
